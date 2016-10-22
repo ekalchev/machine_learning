51367282ace55d5b668c90b927ac51c47f016a39
ekalchev@hotmail.com <ekalchev@hotmail.com>
2016-10-22 04:43:09 -0700

    playing with different configurations

--- oxford_cnn.py
+++ oxford_cnn.py
@@ -6,6 +6,7 @@ from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.estimator import regression
 from os import listdir
 from os.path import isfile, join
+from tflearn.layers.normalization import local_response_normalization
 import ntpath
 
 def load_images(image_path):
@@ -21,8 +22,6 @@ def load_images(image_path):
 
     images = tf.image.decode_jpeg(value, channels=3)
 
-    Y = tf.ones([num_images, 2], tf.int32)
-
     sess = tf.Session()
     sess.run(tf.initialize_all_variables())
     coord = tf.train.Coordinator()
@@ -36,9 +35,9 @@ def load_images(image_path):
       print(path)
       firstLetterOfImageFile = ntpath.basename(path)[:1]
       if(firstLetterOfImageFile == b'd'):
-            labels.append([0,1])
+            labels.append([0, 1])
       else:
-            labels.append([1,0])
+            labels.append([1, 0])
 
       currentImage = sess.run(images)
       imageList.append(currentImage)
@@ -62,46 +61,27 @@ X,Y,_ = load_images("/home/emooo/PycharmProjects/machine_learning/resizedRaspber
 Xtest,Ytest,_ = load_images("/home/emooo/PycharmProjects/machine_learning/resizedRaspberies/fake_test/")
 
 print(Ytest)
-# Building 'VGG Network'
-network = input_data(shape=[None, 80, 80, 3])
 
-network = conv_2d(network, 64, 3, activation='relu')
-network = conv_2d(network, 64, 3, activation='relu')
-network = max_pool_2d(network, 2, strides=2)
-
-network = conv_2d(network, 128, 3, activation='relu')
-network = conv_2d(network, 128, 3, activation='relu')
-network = max_pool_2d(network, 2, strides=2)
-
-network = conv_2d(network, 256, 3, activation='relu')
-network = conv_2d(network, 256, 3, activation='relu')
-network = conv_2d(network, 256, 3, activation='relu')
-network = max_pool_2d(network, 2, strides=2)
-
-network = conv_2d(network, 512, 3, activation='relu')
-network = conv_2d(network, 512, 3, activation='relu')
-network = conv_2d(network, 512, 3, activation='relu')
-network = max_pool_2d(network, 2, strides=2)
-
-network = conv_2d(network, 512, 3, activation='relu')
-network = conv_2d(network, 512, 3, activation='relu')
-network = conv_2d(network, 512, 3, activation='relu')
-network = max_pool_2d(network, 2, strides=2)
-
-network = fully_connected(network, 4096, activation='relu')
-network = dropout(network, 0.5)
-network = fully_connected(network, 4096, activation='relu')
-network = dropout(network, 0.5)
+# Building convolutional network
+network = input_data(shape=[None, 80, 80, 3], name='input')
+network = conv_2d(network, 32, 3, activation='relu', regularizer="L2")
+network = max_pool_2d(network, 2)
+network = local_response_normalization(network)
+network = conv_2d(network, 64, 3, activation='relu', regularizer="L2")
+network = max_pool_2d(network, 2)
+network = local_response_normalization(network)
+network = fully_connected(network, 128, activation='tanh')
+network = dropout(network, 0.8)
+network = fully_connected(network, 256, activation='tanh')
+network = dropout(network, 0.8)
 network = fully_connected(network, 2, activation='softmax')
-
-network = regression(network, optimizer='rmsprop',
-                     loss='categorical_crossentropy',
-                     learning_rate=0.001)
+network = regression(network, optimizer='adam', learning_rate=0.01,
+                     loss='categorical_crossentropy', name='target')
 
 # Training
 model = tflearn.DNN(network, checkpoint_path='model_vgg',
                     max_checkpoints=1, tensorboard_verbose=0)
-model.fit(X, Y, validation_set=0.1, n_epoch=10, shuffle=True,
+model.fit(X, Y, validation_set=0.1, n_epoch=100, shuffle=True,
           show_metric=True, batch_size=64, snapshot_step=500,
           snapshot_epoch=False, run_id='Raspberies')
 
